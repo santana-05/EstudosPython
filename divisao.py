71019072da46fb534f037a7912055fcbdbbc42d0
Santana <mauriciosantanadasilva123@gmail.com>
2025-06-01 18:15:33 +0000

    .

---
+++ divisao.py
@@ -0,0 +1,6 @@
+x = int(input())
+y = int(input())
+
+divisao = x / y
+
+print(divisao)
---
+++ soma.py
@@ -0,0 +1,6 @@
+x = int(input("Digite o primeiro número da soma: "))
+y = int(input("Digite o segundo número da soma: "))
+
+soma = x + y
+
+print("O resultado da soma é:", soma)
